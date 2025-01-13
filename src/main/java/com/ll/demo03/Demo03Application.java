5ba66642a5f5d0fca7f61daba67c93e7b372abe6
Ojiner <dbstjdrud115@gmail.com>
2025-01-14 00:56:32 +0900

    aws cli aceessKey 재발급 후 테스트

--- src/main/java/com/ll/demo03/Demo03Application.java
+++ src/main/java/com/ll/demo03/Demo03Application.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class Demo03Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Demo03Application.class, args);
     }
